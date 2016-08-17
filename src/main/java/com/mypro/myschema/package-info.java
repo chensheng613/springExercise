3c46adb448529d311eb078c029ae72a804c5ee94
chensheng613 <448112062@qq.com>
2016-08-17 18:13:01 +0800

    1

--- src/main/java/com/mypro/myschema/package-info.java
+++ src/main/java/com/mypro/myschema/package-info.java
@@ -2,7 +2,11 @@
 // 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.7 生成的
 // 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // 在重新编译源模式时, 对此文件的所有修改都将丢失。
+<<<<<<< HEAD
 // 生成时间: 2016.08.17 时间 06:05:30 PM CST 
+=======
+// 生成时间: 2016.08.17 时间 05:50:45 PM CST 
+>>>>>>> refs/heads/master
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
